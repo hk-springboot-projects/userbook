3258e67f777c7895dda8c87a7816f39d1acc1485
kumarhemant10 <hkchauhan022@gmail.com>
2023-02-04 20:37:51 -0500

    unused import removed

--- src/test/java/com/hk/prj/userbook/UserControllerTests.java
+++ src/test/java/com/hk/prj/userbook/UserControllerTests.java
@@ -4,32 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hk.prj.userbook.user.User;
 import com.hk.prj.userbook.user.UserController;
 import com.hk.prj.userbook.user.UserService;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.endsWith;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @ExtendWith(SpringExtension.class)
@@ -43,6 +35,7 @@ public class UserControllerTests {
 
     @Test
     void getAllUsers_Test() throws Exception {
+        when(userService.getUsers()).thenReturn(new ArrayList<>());
         mockMvc.perform(get("/users")).andExpect(status().is2xxSuccessful());
     }
 
@@ -52,7 +45,8 @@ public class UserControllerTests {
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(new User(1L, "Hemant", "Kumar"))))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
     }
 
     public static String asJsonString(final Object obj) {
