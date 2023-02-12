f775ad590148af074a55bcd31ceb3d32b2045d19
kumarhemant10 <hkchauhan022@gmail.com>
2023-02-11 22:57:30 -0500

    smoke test added

--- src/test/java/com/hk/prj/userbook/UserbookApplicationTests.java
+++ src/test/java/com/hk/prj/userbook/UserbookApplicationTests.java
@@ -1,13 +1,20 @@
 package com.hk.prj.userbook;
 
+import com.hk.prj.userbook.user.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class UserbookApplicationTests {
 
+	@Autowired
+	UserController userController;
 	@Test
 	void contextLoads() {
+		assertThat(userController).isNotNull();
 	}
 
 }
