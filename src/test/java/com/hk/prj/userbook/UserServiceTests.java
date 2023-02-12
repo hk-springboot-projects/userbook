730c9cfde3a55ff4a4d40aff7eeb02e179daca5e
kumarhemant10 <hkchauhan022@gmail.com>
2023-02-11 23:06:14 -0500

    service test case improved

--- src/test/java/com/hk/prj/userbook/UserServiceTests.java
+++ src/test/java/com/hk/prj/userbook/UserServiceTests.java
@@ -1,19 +1,17 @@
 package com.hk.prj.userbook;
 
 
+import com.hk.prj.userbook.user.User;
 import com.hk.prj.userbook.user.UserRepository;
 import com.hk.prj.userbook.user.UserService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.when;
@@ -27,13 +25,16 @@ public class UserServiceTests {
     @MockBean
     UserRepository userRepository;
 
-    @BeforeEach
-    public void preSetup(){
-        when(userRepository.findAll()).thenReturn(new ArrayList<>());
-    }
 
     @Test
     public void getAllUsers_Test(){
-        assertThat("equals", userService.getUsers().size() == new ArrayList<>().size());
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1L, "Hemant", "Kumar"));
+        userList.add(new User(2L, "H", "K"));
+        when(userRepository.findAll()).thenReturn(userList);
+
+        assertThat("equals", userService.getUsers().size() == 2);
+        assertThat("equals", userService.getUsers().get(0).getId()==1L);
+        assertThat("equals", userService.getUsers().get(1).getId()==2L);
     }
 }
