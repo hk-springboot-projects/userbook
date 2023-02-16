eb8a6d200ac9c313badf7d5201bbef8c20dc2136
kumarhemant10 <hkchauhan022@gmail.com>
2023-02-15 21:55:10 -0500

    repository test - save all users

--- src/test/java/com/hk/prj/userbook/UserRepositoryTest.java
+++ src/test/java/com/hk/prj/userbook/UserRepositoryTest.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -25,12 +28,6 @@ public class UserRepositoryTest {
     }
 
     @Test
-    @DisplayName("Empty find all")
-    public void testEmptyFindAll(){
-        assertThat(userRepository.findAll().size()).isEqualTo(0);
-    }
-
-    @Test
     @DisplayName("save user")
     public void testSaveUser_success(){
         User user = User.builder().firstName("Hemant").lastName("Kumar").build();
@@ -39,6 +36,25 @@ public class UserRepositoryTest {
         assertThat(savedUser.getFirstName().equalsIgnoreCase("Hemant"));
         assertThat(savedUser.getLastName().equalsIgnoreCase("Kumar"));
         assertThat(savedUser.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("save users list")
+    public void testSaveUsers_success(){
+        User user1 = User.builder().firstName("Hemant").lastName("Kumar").build();
+        User user2 = User.builder().firstName("User 1").lastName("last name").build();
+        User user3 = User.builder().firstName("User 2").lastName("last name").build();
+        List<User> savedUsers = userRepository.saveAll(Arrays.asList(user1, user2, user3));
+        assertThat(savedUsers.size() > 0);
+        assertThat(savedUsers.get(0).getFirstName().equalsIgnoreCase("Hemant"));
+        assertThat(savedUsers.get(0).getLastName().equalsIgnoreCase("Kumar"));
+        assertThat(savedUsers.get(0).getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("find all")
+    public void testEmptyFindAll(){
+        assertThat(userRepository.findAll().size()).isNotEqualTo(0);
     }
 
 }
