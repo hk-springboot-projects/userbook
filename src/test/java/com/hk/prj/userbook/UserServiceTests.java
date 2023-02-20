f8ff5550915c62ae2c0eedee1d56a905009a0930
kumarhemant10 <hkchauhan022@gmail.com>
2023-02-19 21:21:54 -0500

    service test user by id pass and fail scenarios

--- src/test/java/com/hk/prj/userbook/UserServiceTests.java
+++ src/test/java/com/hk/prj/userbook/UserServiceTests.java
@@ -1,12 +1,16 @@
 package com.hk.prj.userbook;
 
 
+import com.hk.prj.userbook.user.User;
+import com.hk.prj.userbook.user.UserNotFoundException;
 import com.hk.prj.userbook.user.UserRepository;
 import com.hk.prj.userbook.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
@@ -22,8 +26,28 @@ public class UserServiceTests {
 
 
     @Test
-    public void getAllUsers_Test(){
+    public void getAllUsers_Test() {
         when(userRepository.findAll()).thenReturn(UserUtil.getUsers());
         assertThat(userService.getUsers().size()).isNotEqualTo(0);
+    }
+
+    @Test
+    public void getUsersById_success() {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(UserUtil.getUsers().get(0)));
+        User user = userService.getUserById(1L);
+        assertThat(user.getFirstName()).isEqualTo("H1");
+        assertThat(user.getLastName()).isEqualTo("K1");
+    }
+
+    @Test
+    public void getUsersById_failed() {
+        when(userRepository.findById(2L)).thenThrow(new UserNotFoundException("User not found with id 2"));
+        try {
+            userService.getUserById(2L);
+        } catch (UserNotFoundException u) {
+            assertThat(u).isNotNull();
+            assertThat(u.getMessage()).isEqualTo("User not found with id 2");
+        }
+
     }
 }
