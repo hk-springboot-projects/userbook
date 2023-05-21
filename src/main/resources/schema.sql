7121a33ddce0a6019e7061d37dfc0c25bfeb2d2f
kumarhemant10 <hkchauhan022@gmail.com>
2023-05-20 20:46:13 -0400

    h2 and schema update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,11 @@
-create table user_detail (id bigint not null AUTO_INCREMENT, first_name varchar(255), last_name varchar(255), primary key (id));
+create table user_detail
+(
+    id bigint not null AUTO_INCREMENT,
+    first_name varchar(255),
+    last_name varchar(255),
+    email varchar(255),
+    gender varchar(10),
+    city varchar(255),
+    country varchar(255),
+    primary key (id)
+);
